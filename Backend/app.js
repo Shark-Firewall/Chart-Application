34d76f361db152da0a27cc1e88adfe36879dfcff
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2022-07-03 15:34:46 +0530

    frontend part done!

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("listening on port " + process.env.PORT);
+});
---
+++ frontend/src/componets/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+function Header() {
+  return (
+    <div className="brand">
+      <imp src="nbgy" alt="image" />
+      <h4>Shark</h4>
+    </div>
+  );
+}
+
+export default Header;
