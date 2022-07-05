1dbc5b1097272c302179bb918602a64efc09c914
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2022-07-05 13:33:14 +0530

    Initial setup for server

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http); 
+const cors = require("cors");
 require("dotenv").config();
+
+// creating server configuration
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-app.listen(process.env.PORT, () => {
+http.listen(process.env.PORT, () => {
   console.log("listening on port " + process.env.PORT);
 });
