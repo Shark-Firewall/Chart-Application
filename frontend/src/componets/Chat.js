673b1324714c4d7fb760549cdbb809a1daebac1c
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2022-07-24 17:22:43 +0530

    sending data from one page to another

--- frontend/src/componets/Chat.js
+++ frontend/src/componets/Chat.js
@@ -3,8 +3,18 @@ import "../App.css";
 import Header from "./Header";
 import Body from "./Body";
 import Base from "./Base";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function Chat() {
+  const [user, setuser] = useState("");
+  let location = useLocation();
+
+  useEffect(() => {
+    setuser(location.state);
+  }, [location]);
+
+  console.log(user);
   return (
     <div className="body">
       <div className="App">
